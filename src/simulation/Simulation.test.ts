ca517f9d0e898e629f9cc68e3c6b355151d8d825
Richard Teammco <teammcr192@gmail.com>
2021-12-28 21:32:09 -0500

    Added custom matrix comparator and initial test is fixed

--- src/simulation/Simulation.test.ts
+++ src/simulation/Simulation.test.ts
@@ -1,12 +1,43 @@
-import React from 'react';
 import { runSimulationStep } from './Simulation';
-import { initialSimulationState, SimulationState } from './SimulationData';
+import { initialSimulationState, SimulationMatrix } from './SimulationData';
+
+// Matrix approximate value comparator since the calcluations aren't exact:
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      matrixToBeCloseTo(value: SimulationMatrix): CustomMatcherResult;
+    }
+  }
+}
+expect.extend({
+  matrixToBeCloseTo(expected: SimulationMatrix, actual: SimulationMatrix) {
+    for (let i = 0; i < expected.length; i++) {
+      const expectedRow = expected[i];
+      const actualRow = actual[i];
+      for (let j = 0; j < expectedRow.length; j++) {
+        const expectedVal = expectedRow[j];
+        const actualVal = actualRow[j];
+        if (Math.abs(expectedVal - actualVal) > 0.005) {
+          return {
+            message: () =>
+              `Incorrect matrix value at row ${i}, col ${j}: expected ${expectedVal}, got ${actualVal}`,
+            pass: false,
+          };
+        }
+      }
+    }
+    return {
+      message: () => 'Matrices were roughly equal',
+      pass: true,
+    };
+  },
+});
 
 test('runSimulationStep predicts no changes without movement or noise', () => {
   const simulationState = initialSimulationState;
   const { predictedState, predictedCovariance } = runSimulationStep(simulationState);
   expect(predictedState).toEqual([0, 0, 0, 0]);
-  expect(predictedCovariance).toEqual([
+  expect(predictedCovariance).matrixToBeCloseTo([
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
